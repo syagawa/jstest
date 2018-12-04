75d3089c090f81a46d3bcedcecf3ab6675e726c0
yagawa <yagawa@qzc.co.jp>
2018-12-04 11:43:03 +0900

    Update babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,11 @@ module.exports = {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: "babel-loader",
-          query: {
-            presets: ["es2015"]
+          use:{
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
           }
         }
       ]
